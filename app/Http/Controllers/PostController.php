fe51c03c83b3f7bc672fe19cc82a4da39568bc7b
km-mainPC <kmatsumoto.fw@gmail.com>
2024-10-22 22:59:05 +0900

    added post index, indivisual display, edit, delete functions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,8 +17,15 @@ class PostController extends Controller
         return view('post.create');
     }
 
+    public function show (Post $post) {
+        return view('post.show', compact('post'));
+    }
+
+    public function edit(Post $post) {
+        return view('post.edit', compact('post'));
+    }
+
     public function store(Request $request) {
-        Gate::authorize('test');
         
         $validated = $request->validate([
             'title' => 'required|max:20',
@@ -35,5 +42,26 @@ class PostController extends Controller
         // ]);
         $request->session()->flash('message', '保存しました');
         return back();
+    }
+
+    public function update(Request $request, Post $post) {
+        
+        $validated = $request->validate([
+            'title' => 'required|max:20',
+            'body' => 'required|max:400',
+        ]);
+
+        $validated['user_id'] = auth()->id();
+        
+        $post->update($validated);
+
+        $request->session()->flash('message', '更新しました');
+        return back();
+    }
+
+    public function destroy(Request $request, Post $post) {
+        $post->delete();
+        $request->session()->flash('message', '削除しました');
+        return redirect()->route('post.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,14 @@ Route::get('post/create', [PostController::class, 'create']);
 
 Route::post('post', [PostController::class, 'store'])->name('post.store');
 
-Route::get('post', [PostController::class, 'index']);
+Route::get('post', [PostController::class, 'index'])->name('post.index');
+
+Route::get('post/show/{post}', [PostController::class, 'show'])->name('post.show');
+
+Route::get('post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
+Route::patch('post/{post}', [PostController::class, 'update'])->name('post.update');
+
+Route::delete('post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
